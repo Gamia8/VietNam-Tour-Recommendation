f31758a1b23da71bbb034221d052f96730ef6ea4
KietLG <lamgiakiet16032004@gmail.com>
2025-09-19 23:08:58 +0700

    feat: add Vietnam tourist destinations dataset by categorie

---
+++ Place_crawl.py
@@ -0,0 +1,78 @@
+bay = [
+    'Vịnh Hạ Long', 'Vịnh Lan Hạ', 'Vịnh Bái Tử Long', 'Vịnh Vân Phong', 'Vịnh Ninh Vân', 'Vịnh Xuân Đài', 'Vịnh Vũng Rô', 'Vịnh Cam Ranh', 'Vịnh Nha Trang', 'Vịnh Lăng Cô', 
+    'Vịnh Vĩnh Hy', 'Vịnh Hà Tiên', 'Vịnh Rạch Giá', 'Vịnh Phú Quốc', 'Vịnh Nam Du', 'Vịnh Cửa Lục', 'Vịnh Tịnh Gia', 'Vịnh Cửa Hội', 'Vịnh Cửa Tùng', 'Vịnh Thuận An', 
+    'Vịnh Chân Mây', 'Vịnh An Hòa', 'Vịnh Dung Quất', 'Vịnh Quy Nhơn', 'Vịnh Tam Quan', 'Vịnh Gành Rái', 'Vịnh Mũi Né', 'Vịnh Hòn Rơm', 'Vịnh Cà Ná', 'Vịnh Bình Tiên'
+]
+cave = [
+    'Hang Sơn Đoòng', 'Hang Thiên Đường', 'Hang Phong Nha', 'Hang Tiên Sơn', 'Hang Tù Làn', 'Hang Én', 'Hang Va', 'Hang Tối', 'Tam Cốc', 'Hang Tràng An', 'Hang Nước Nứt', 
+    'Hang Sửng Sốt', 'Hang Luồn', 'Hang Đầu Gỗ', 'Hang Bích Động', 'Hang Múa', 'Hang Thủy Cung', 'Hang Hương Tích', 'Hang Cốc Bốc', 'Hang Động Cung', 
+    'Hang Pác Bó', 'Hang Thẩm Khuyên', 'Hang Thung', 'Hang Chùa', 'Hang Rục Mòn', 'Hang Rái', 'Hang Dơi', 'Hang Tám Cô', 'Hang Tiên', 'Hang Ba'
+]
+
+historical_relics = [
+    'Kinh Thành Huế', 'Thánh Địa Mỹ Sơn', 'Địa Đạo Củ Chi', 'Lăng Chủ Tịch Hồ Chí Minh', 'Văn Miếu Quốc Tử Giám', 'Thành Nhà Hồ', 'Địa Đạo Vịnh Mốc', 
+    'Bảo Tàng Chứng Tích Chiến Tranh', 'Dinh Thống Nhất', 'Phố Cổ Hà Nội', 'Thành Cổ Loa', 'Bảo Tàng Cổ Vật Cung Đình Huế', 'Dinh Bảo Đại', 'Bảo Tàng Chăm', 
+    'Bảo Tàng Thành Phố Hồ Chí Minh', 'Bảo Tàng Dân Tộc Học Việt Nam', 'Cầu Thê Húc', 'Đền Hùng', 'Hoàng Thành Thăng Long', 'Khu Di Tích Pác Bó', 'Khu Di Tích Tân Trào', 
+    'Nhà Tù Côn Đảo', 'Khu Di Tích K9', 'Nhà Tù Hỏa Lò', 'Đền Ngọc Sơn', 'Khu Di Tích Bạch Đằng', 'Lăng Tự Đức', 'Đền Trần', 'Khu Di Tích Cố Đô Hoa Lư'
+]
+pagoda = [
+    'Chùa Một Cột', 'Chùa Hương', 'Chùa Trấn Quốc', 'Chùa Thiên Mụ', 'Chùa Từ Đàm', 'Chùa Báo Quốc', 'Chùa Diệu Đế', 'Chùa Tịnh Tâm', 'Chùa Giác Hoàng', 'Chùa Từ Hiếu', 
+    'Chùa Ngọc Hoàng', 'Chùa Xá Lợi', 'Chùa Giác Lâm', 'Chùa Bà Chúa Xứ', 'Chùa Hộ Quốc', 'Chùa Tam Chúc', 'Chùa Linh Ứng', 'Chùa Pháp Hoa', 'Chùa Bảo Lâm', 'Chùa Đại Tòng Lâm', 
+    'Chùa Linh Sơn', 'Chùa Kéo', 'Chùa Hà', 'Chùa Bái Đính', 'Chùa Vĩnh Nghiêm', 'Chùa Bà Thiên Hậu', 'Chùa Phước Hải', 'Chùa Huệ Nghiêm', 'Chùa Hoằng Phúc', 
+    'Chùa Tôn Thạnh', 'Chùa Vĩnh Phước', 'Chùa Quan Công', 'Chùa Phù Dung', 'Chùa Thiền Lâm', 'Chùa Hội Khánh', 'Chùa Long Thọ', 'Chùa Long Sơn', 'Chùa Kim Liên', 'Chùa Nam Nhất', 'Chùa An Linh'
+]
+lake = [
+    'Hồ Hoàn Kiếm', 'Hồ Tây', 'Hồ Ba Bể', 'Hồ Thác Bà', 'Hồ Đại Lải', 'Hồ Tuyên Quang', 'Hồ Tô Nung', 'Hồ Núi Cốc', 'Hồ Trúc Bạch', 'Hồ Tuyền Lâm', 'Hồ Xuân Hương', 
+    'Hồ Đắk Mil', 'Hồ Dầu Tiếng', 'Hồ Trị An', 'Hồ Phú Ninh', 'Hồ Đồng Nai', 'Hồ Suối Hai', 'Hồ Văn', 'Hồ Biển Lắc', 'Hồ Lắk', 'Hồ Tà Đùng', 'Hồ Hòa Bình', 
+    'Hồ Thang Hen', 'Hồ Sông Mực', 'Hồ Kẻ Gỗ', 'Hồ Quan Sơn', 'Hồ Na Hang', 'Hồ Thác Bạc', 'Hồ Nước Trong', 'Hồ Ngọc'
+]
+waterfall = [
+    'Thác Bản Giốc', 'Thác Pongour', 'Thác Voi', 'Thác Cam Ly', 'Thác Prenn', 'Thác Yang Bay', 'Thác Ba Ho', 'Thác Dray Nur', 'Thác Dambri', 'Thác Đắk G’lun',
+    'Thác Đray Sáp', 'Thác Gia Long', 'Thác Trinh Nữ', 'Thác Thủy Tiên', 'Thác Hang Én', 'Thác Mơ', 'Thác Chiêu', 'Thác Tà Gân', 'Thác Cát Cát', 
+    'Thác Bạc', 'Thác Tình Yêu', 'Thác Dải Yếm', 'Thác Nàng', 'Thác Bà Thước', 'Thác Grăng', 'Thác Đắk N’Tăng', 'Thác Lưu Ly', 'Thác Chín Tầng', 'Thác Khuổi Nhi', 'Thác Mây'
+]
+river = [
+    'Sông Hồng', 'Sông Cửu Long', 'Sông Đà', 'Sông Hương', 'Sông Thu Bồn', 'Sông Lô', 'Sông Gianh', 'Sông Bến Hải', 'Sông Đồng Nai', 'Sông Sài Gòn', 'Sông Nho Quế', 
+    'Sông Hàn', 'Sông Hoài', 'Sông Sêrêpốk', 'Sông Ba', 'Sông Trà Khúc', 'Sông Vàm Cỏ Đông', 'Sông Vàm Cỏ Tây', 'Sông Cả', 'Sông Kôn', 'Sông Tiền', 'Sông Hậu', 'Sông Son', 
+    'Sông Ngô Đồng', 'Sông Cầu', 'Sông Thạch Hãn', 'Sông La', 'Sông Vĩnh Định', 'Sông Bạch Đằng', 'Sông Lục Nam'
+]
+jungle = [
+    'Rừng Cúc Phương', 'Rừng Ba Bể', 'Rừng Cát Tiên', 'Rừng Yok Đôn', 'Rừng Tràm Chim', 'Rừng U Minh Thượng', 'Rừng U Minh Hạ', 'Rừng Nam Cát Tiên', 'Rừng Pù Luông', 
+    'Rừng Phong Nha', 'Rừng Bạch Mã', 'Rừng Bidoup Núi Bà', 'Rừng Côn Đảo', 'Rừng Nam Nung', 'Rừng Tà Đùng', 'Rừng Hoàng Liên', 'Rừng Xuân Sơn', 
+    'Rừng Chư Yang Sin', 'Rừng Tà Năng', 'Rừng Mã Đà', 'Rừng Phước Bình', 'Rừng Kon Ka Kinh', 'Rừng Ba Vì', 'Rừng Tam Đảo', 'Rừng Cần Giờ', 
+    'Rừng Lăng Quăng', 'Rừng Đắk Uy', 'Rừng Vũ Quang', 'Rừng Pù Mát', 'Rừng Bù Gia Mập'
+]
+beach = [
+    'Biển Mũi Né', 'Biển Nha Trang', 'Biển Mỹ Khê', 'Biển Quy Nhơn', 'Biển Sầm Sơn', 'Biển Cửa Lò', 'Biển Non Nước', 'Biển Nhật Lệ', 'Biển Lăng Cô', 'Biển Vĩnh Hy', 'Biển Cửa Đại', 
+    'Biển Hà My', 'Biển Cần Giờ', 'Biển Vũng Tàu', 'Biển Cà Ná', 'Biển Thùy Vân', 'Biển Dốc Lết', 'Biển Cửa Việt', 'Biển Mân Thái', 'Biển Lộc An', 'Biển Long Hải', 'Biển Hàu', 'Biển Thạnh Phú', 
+    'Biển Đồng Châu', 'Biển Xuân Thiều', 'Biển Trà Cổ', 'Biển Tĩnh Gia', 'Biển Hải Hòa', 'Biển Hải Thịnh', 'Biển Bãi Lữ', 'Biển Thiên Cầm', 'Biển Cửa Tùng', 'Biển Thuận An', 'Biển Cảnh Dương', 
+    'Biển Tân Thành', 'Biển Phước Hải', 'Biển Hồ Cốc', 'Biển Biển Quát', 'Biển Suối Ồ', 'Biển Bình Châu', 'Biển Gò Công', 'Biển Tân Thành (Tiền Giang)', 'Biển Ba Động', 'Biển Vĩnh Châu', 
+    'Biển Mỏ Ó', 'Biển Hòn Rơm', 'Biển Cam Bình', 'Biển Đại Lãnh', 'Biển Kỳ Ninh', 'Biển Nam Ô'
+]
+mountain = [
+    'Fansipan', 'Bạch Mộc Lương Tử', 'Pha Luông', 'Pu Si Lung', 'Tà Xùa', 'Yên Tử', 'Bà Nà Hills', 'Lang Biang', 'Núi Bà Đen', 'Núi Tà Cú', 
+    'Núi Sập', 'Núi Sam', 'Núi Dinh', 'Núi Chứa Chan', 'Núi Thần Tài', 'Núi Bạch Mã', 'Núi Hồng Lĩnh', 'Núi Ngọc Linh', 'Núi Hàm Rồng', 'Núi Mẫu Sơn', 'Núi Pu Ta Leng', 
+    'Núi Đá Bia', 'Núi Cấm', 'Núi Cô Tô', 'Núi Ba Vì', 'Núi Tam Đảo', 'Núi Ngũ Chỉ Sơn', 'Núi Đỉnh Chu', 'Núi ngũ Hành Sơn'
+]
+island = [
+    'Phú Quốc', 'Côn Đảo', 'Cát Bà', 'Cô Tô', 'Nam Du', 'Bình Ba', 'Phú Quý', 'Lý Sơn', 'Bình Hưng', 'Hòn Tằm', 'Hòn Tre', 'Hòn Mun', 'Hòn Chồng', 'Hòn Rái', 'Hòn Nghệ', 
+    'Hòn Ngư', 'Hòn Mắt', 'Hòn Oản', 'Hòn Khô', 'Hòn Đất', 'Hòn Bà', 'Hòn Dáu', 'Hòn Bạch Long Vĩ', 'Hòn Cồn Cỏ', 'Hòn Sơn', 
+    'Hòn Me', 'Hòn Tý', 'Hòn Nưa', 'Hòn Chùa', 'Hòn Khoai', 'Hòn Chuối', 'Hòn Đốc', 'Hòn Lại Sơn', 'Hòn Thơm', 'Hòn Mây Rút', 'Hòn Gầm Ghì', 'Hòn Một', 'Hòn Hai', 
+    'Hòn Ba', 'Hòn Rùa', 'Hòn Yến', 'Hòn Tăng', 'Hòn Trứng', 'Hòn Dừa', 'Hòn Bảy Cạnh', 'Hòn Cau', 'Hòn Tài', 'Hòn Trác', 'Hòn Đồi Mồi'
+]
+
+def get_place_list():
+    places = (
+        [(name, "bay") for name in bay] +
+        [(name, "cave") for name in cave] +
+        [(name, "historical_relics") for name in historical_relics] +
+        [(name, "pagoda") for name in pagoda] +
+        [(name, "lake") for name in lake] +
+        [(name, "waterfall") for name in waterfall] +
+        [(name, "river") for name in river] +
+        [(name, "jungle") for name in jungle] +
+        [(name, "beach") for name in beach] +
+        [(name, "mountain") for name in mountain] +
+        [(name, "island") for name in island]
+    )
+    return places
